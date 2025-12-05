90342b9c39e52eb17e58897652b509f5146c670f
Ayz <120428408+ayz-x@users.noreply.github.com>
2025-12-05 14:38:17 -0800

    Update SudokuChecker.java

--- SudokuChecker.java
+++ SudokuChecker.java
@@ -140,7 +140,7 @@ public class SudokuChecker {
     //example of one using two nested loops (unused in this program)
     public static boolean checkWithTwoLoops(int[] list){
         for(int i = 0; i < list.length; i++){
-            for(int j = i; j < list.length; j++){
+            for(int j = i+1; j < list.length; j++){
                 if(list[i] == list[j]){
                     return false;
                 }
@@ -149,5 +149,6 @@ public class SudokuChecker {
         return true;
     }
 }
+
 
 
